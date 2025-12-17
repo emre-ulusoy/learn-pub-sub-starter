b15e68a11a2944bd83563df86a9b143d55eaea27
Emre Ulusoy <emre5tornaci@gmail.com>
2025-12-16 22:15:40 -0600

    ch3 l4, publishers and queues

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,6 +8,8 @@ import (
 	"runtime"
 	"syscall"
 
+	pubsub "github.com/bootdotdev/learn-pub-sub-starter/internal"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -21,12 +23,21 @@ func main() {
 	}
 
 	defer conn.Close()
-
 	fmt.Println("connection successful")
+
+	newChan, err := conn.Channel()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	state := routing.PlayingState{
+		IsPaused: true,
+	}
+	pubsub.PublishJSON[routing.PlayingState](newChan, routing.ExchangePerilDirect, routing.PauseKey, state)
 
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-sigChan
+	<-sigChan // INFO: Blocker
 	fmt.Println("signal received, shutting down the connection")
 	conn.Close()
 
